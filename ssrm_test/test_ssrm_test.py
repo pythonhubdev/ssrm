bf8a20e9dd2684898ad870ce22676d57506ee772
Sammy Lee <sammy.lee@optimizely.com>
2020-05-08 19:34:18 -0700

    test(stats): add missing tests for methods in srm_test

--- ssrm_test/test_ssrm_test.py
+++ ssrm_test/test_ssrm_test.py
@@ -11,6 +11,7 @@ from .ssrm_test import (
     multinomiallogpmf,
     posterior_probability,
     sequential_posteriors,
+    srm_test
 )
 
 
@@ -59,3 +60,107 @@ def test_multinomiallogpmf():
     ps = [[0.2, 0.4, 0.4], [1 / 3, 1 / 3, 1 / 3], [0.3, 0.4, 0.3], [0, 0, 1]]
     for x, n, p in zip(xs, ns, ps):
         assert multinomial.logpmf(x, n, p) == approx(multinomiallogpmf(x, n, p))
+
+
+def test_sequential_posteriors():
+    # Testing unit-level data, where the counts across all variations are represented as one-hot vectors.
+    expected_posteriors_dict = [
+        {
+            "log_marginal_likelihood_M1": -1.0986122886681096,
+            "log_marginal_likelihood_M0": -0.916290731874155,
+            "posterior_M1": np.array([2, 1, 1]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -2.4849066497880004,
+            "log_marginal_likelihood_M0": -1.83258146374831,
+            "posterior_M1": np.array([2, 2, 1]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -3.401197381662155,
+            "log_marginal_likelihood_M0": -2.748872195622465,
+            "posterior_M1": np.array([3, 2, 1]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -4.499809670330265,
+            "log_marginal_likelihood_M0": -3.66516292749662,
+            "posterior_M1": np.array([3, 3, 1]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -6.4457198193855785,
+            "log_marginal_likelihood_M0": -5.27460083993072,
+            "posterior_M1": np.array([3, 3, 2]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+    ]
+    datapoints = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    null_probabilities = np.array([0.4, 0.4, 0.2])
+    posteriors_dict = sequential_posteriors(datapoints, null_probabilities)
+    assert len(posteriors_dict) == len(datapoints)
+    for acc_dict, expected_acc_dict in zip(posteriors_dict, expected_posteriors_dict):
+        assert acc_dict["log_marginal_likelihood_M1"] == approx(
+            expected_acc_dict["log_marginal_likelihood_M1"]
+        )
+        assert acc_dict["log_marginal_likelihood_M0"] == approx(
+            expected_acc_dict["log_marginal_likelihood_M0"]
+        )
+        assert acc_dict["posterior_M1"] == approx(expected_acc_dict["posterior_M1"])
+        assert acc_dict["posterior_M0"] == approx(expected_acc_dict["posterior_M0"])
+
+    # Testing time-aggregated data, where each entry represents an aggregate count across all variations for that time.
+    datapoints = np.array([[20, 17, 9], [18, 21, 8], [4, 6, 4], [18, 19, 11]])
+    null_probabilities = np.array([0.4, 0.4, 0.2])
+    posteriors_dict = sequential_posteriors(datapoints, null_probabilities)
+    expected_posteriors_dict = [
+        {
+            "log_marginal_likelihood_M1": -7.028201432058012,
+            "log_marginal_likelihood_M0": -4.0776406466061985,
+            "posterior_M1": np.array([21, 18, 10]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -11.947847738358888,
+            "log_marginal_likelihood_M0": -8.265946861099877,
+            "posterior_M1": np.array([39, 39, 18]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -15.471401950257842,
+            "log_marginal_likelihood_M0": -11.610743519545139,
+            "posterior_M1": np.array([43, 45, 22]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+        {
+            "log_marginal_likelihood_M1": -19.96484521090386,
+            "log_marginal_likelihood_M0": -15.781031228536165,
+            "posterior_M1": np.array([61, 64, 33]),
+            "posterior_M0": np.array([0.4, 0.4, 0.2]),
+        },
+    ]
+    assert len(posteriors_dict) == len(datapoints)
+    for acc_dict, expected_acc_dict in zip(posteriors_dict, expected_posteriors_dict):
+        assert acc_dict["log_marginal_likelihood_M1"] == approx(
+            expected_acc_dict["log_marginal_likelihood_M1"]
+        )
+        assert acc_dict["log_marginal_likelihood_M0"] == approx(
+            expected_acc_dict["log_marginal_likelihood_M0"]
+        )
+        assert acc_dict["posterior_M1"] == approx(expected_acc_dict["posterior_M1"])
+        assert acc_dict["posterior_M0"] == approx(expected_acc_dict["posterior_M0"])
+
+
+def test_srm_test():
+    # Testing unit-level data, where the counts across all variations are represented as one-hot vectors.
+    datapoints = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    null_probabilities = np.array([0.4, 0.4, 0.2])
+    mismatch_prob = srm_test(datapoints, null_probabilities)
+    assert mismatch_prob >= 0 and mismatch_prob <= 1
+
+    # Testing time-aggregated data, where each entry represents an aggregate count across all variations for that time.
+    datapoints = np.array([[20, 17, 9], [18, 21, 8], [4, 6, 4], [18, 19, 11]])
+    null_probabilities = np.array([0.4, 0.4, 0.2])
+    mismatch_prob = srm_test(datapoints, null_probabilities)
+    assert mismatch_prob >= 0 and mismatch_prob <= 1

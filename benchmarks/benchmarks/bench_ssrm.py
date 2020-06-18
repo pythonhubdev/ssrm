c1538bd1cf49f9565f4de6c1e70b185f6666f1f9
Mandy Trinh <mandy.trinh@optimizely.com>
2020-06-18 16:09:06 -0700

    test: write remaining benchmark tests

--- benchmarks/benchmarks/bench_ssrm.py
+++ benchmarks/benchmarks/bench_ssrm.py
@@ -1,40 +1,79 @@
+from functools import reduce
+
+import numpy as np
 from scipy.stats import multinomial
 from ssrm_test import ssrm_test as st
-
-# # Set the seed of our random number generator for reproducibility. Don't worry about this
-# np.random.seed(0)
-
-# # Our intended allocation probabilities
-# p_0 = [0.1, 0.5, 0.4]
-
-# # The actual allocation probabilities
-# p = [0.1, 0.49, 0.41]
-
-# # Specify number of visitors
-# n = 10000
-
-# # Generate allocations
-# data = multinomial.rvs(1, p, size=n)
 
 
 class SSRMSuite:
     def setup(self):
         self.intended_p_alloc = [0.1, 0.5, 0.4]
         self.actual_p_alloc = [0.1, 0.49, 0.41]
-        self.n = 10000
-        self.data = multinomial.rvs(1, self.actual_p_alloc, size=self.n)
+        self.n = 1000
+        self.alpha = 0.01
+        self.dirichlet_probability = np.array([1, 3, 2])
+        self.dirichlet_concentration = 1
+        self.dirichlet_alpha = self.dirichlet_probability * self.dirichlet_concentration
+        self.observations = multinomial.rvs(1, self.actual_p_alloc, size=self.n)
+        self.observations_sum = reduce(lambda x, y: x + y, self.observations)
 
     def time_sequential_posteriors(self):
-        st.sequential_posteriors(self.data, self.intended_p_alloc)
+        st.sequential_posteriors(
+            self.observations,
+            self.intended_p_alloc,
+            dirichlet_probability=self.dirichlet_probability,
+            dirichlet_concentration=self.dirichlet_concentration,
+        )
+
+    # def time_total_n(self):
+    #     posterior = {
+    #     "log_marginal_likelihood_M1": -23.585991739528254,
+    #     "log_marginal_likelihood_M0": -76546.65811250894,
+    #     "posterior_M1": [88519.5, 12540.25, 13002.25],
+    #     "posterior_M0": [0.75, 0.125, 0.125],
+    #     }
+    #     st.total_n(posterior)
+
+    def time_bayes_factor(self):
+        posterior = {
+            "log_marginal_likelihood_M1": -23.585991739528254,
+            "log_marginal_likelihood_M0": -76546.65811250894,
+            "posterior_M1": [88519.5, 12540.25, 13002.25],
+            "posterior_M0": [0.75, 0.125, 0.125],
+        }
+        st.bayes_factor(posterior)
+
+    def time_posterior_probability(self):
+        final_posterior = st.sequential_posteriors(
+            self.observations,
+            self.actual_p_alloc,
+            dirichlet_probability=self.dirichlet_probability,
+            dirichlet_concentration=self.dirichlet_concentration,
+        )[-1]
+        final_bayes_factor = st.bayes_factor(final_posterior)
+        st.posterior_probability(final_bayes_factor)
+
+    def time_log_marginal_likelihood_M1(self):
+        st.log_posterior_predictive(self.observations_sum, self.dirichlet_alpha)
+
+    def time_log_marginal_likelihood_M0(self):
+        st.multinomiallogpmf(
+            self.observations_sum, self.observations_sum.sum(), self.actual_p_alloc
+        )
+
+    def time_get_bayes_factor_threshold(self):
+        st.get_bayes_factor_threshold(
+            self.observations_sum, self.dirichlet_alpha, self.alpha
+        )
 
     def time_sequential_posterior_probabilities(self):
-        st.sequential_posterior_probabilities(self.data, self.intended_p_alloc)
+        st.sequential_posterior_probabilities(self.observations, self.intended_p_alloc)
 
     def time_sequential_p_values(self):
-        st.sequential_p_values(self.data, self.intended_p_alloc)
+        st.sequential_p_values(self.observations, self.intended_p_alloc)
 
     def time_sequential_bayes_factors(self):
-        st.sequential_bayes_factors(self.data, self.intended_p_alloc)
+        st.sequential_bayes_factors(self.observations, self.intended_p_alloc)
 
     def time_srm_test(self):
-        st.srm_test(self.data, self.intended_p_alloc)
+        st.srm_test(self.observations, self.intended_p_alloc)

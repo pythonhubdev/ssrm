3a68c5e31b868afc5a901d122e765bc20a95a215
Ajith Mascarenhas <ajith@optimizely.com>
2020-06-22 22:37:40 -0700

    test: deletes notebook test dir

--- ssrm_test/test_jupyter_notebooks.py
+++ ssrm_test/test_jupyter_notebooks.py
@@ -1,5 +1,20 @@
+#     Copyright 2020 Optimizely Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
 import glob
 import os
+import shutil
 from typing import Any, Union
 
 import papermill as pm
@@ -19,7 +34,10 @@ def generate_papermill_output_dir(tmpdir_factory: object) -> object:
     """
     try:
         os.makedirs(JUPYTER_NOTEBOOK_TESTING_OUTPUT_DIR, exist_ok=True)
-        return JUPYTER_NOTEBOOK_TESTING_OUTPUT_DIR
+        yield JUPYTER_NOTEBOOK_TESTING_OUTPUT_DIR
+
+        # Teardown: delete testing output dir.
+        shutil.rmtree(JUPYTER_NOTEBOOK_TESTING_OUTPUT_DIR)
     except OSError as err:
         raise err
 
